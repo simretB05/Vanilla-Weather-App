641e31cab17537eeb93a2d4d1561bbb7c79a861e
simretB05 <simretB05@users.noreply.github.com>
2021-04-18 15:14:25 -0600

    icon update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-let city = "addis ababa";
+let city = "paris";
 let apiKey = "dbdeefe04c94b8d2607c00ef173913af";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
 function showTemperature(response) {
   console.log(response.data);
   let temperature = Math.round(response.data.main.temp);
-
   let temperatureElement = document.querySelector("#temperature");
   let description = document.querySelector("#temperature-description");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#windSpeed");
   let iconElement = document.querySelector("#icon");
-
   description.innerHTML = response.data.weather[0].description;
   temperatureElement.innerHTML = `${temperature}`;
   humidityElement.innerHTML = response.data.main.humidity;
